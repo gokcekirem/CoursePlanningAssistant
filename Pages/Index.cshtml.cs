ec6ae272667491adf0673a922ce887fb0f3bfbc6
Irem Gokcek <gokcek.h.i@gmail.com>
2020-04-09 22:28:15 +0300

    added json deserializer for buble_data.json

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace CoursePlanner.Pages
@@ -15,6 +17,8 @@ namespace CoursePlanner.Pages
 
         public JsonResult json;
 
+        public JArray jarray;
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -23,6 +27,14 @@ namespace CoursePlanner.Pages
         public void OnGet()
         {
             //Fill json here
+            using (StreamReader file = System.IO.File.OpenText(@"BubbleData\bubble_data.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                jarray = (JArray)serializer.Deserialize(file, typeof(JArray));
+                //Console.WriteLine(jarray);
+            }
+            //json = jarray;
+
         }
     }
 }
